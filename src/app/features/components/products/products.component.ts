166ad0bdf40d3eb4e3135857f72802fefd4fff88
Habibamohamed2004 <hamedhabiba217@gmail.com>
2026-02-25 22:37:49 +0200

    Done Products

--- src/app/features/components/products/products.component.ts
+++ src/app/features/components/products/products.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Product } from '../../../shared/models/Iproduct';
+import { ProductService } from '../../services/product/product.service';
+import { ProductcardComponent } from '../../../shared/components/productcard/productcard.component';
+import { FormsModule } from '@angular/forms';
+import { SearchproductPipe } from '../../../shared/pipes/searchproduct/searchproduct-pipe';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ProductcardComponent,SearchproductPipe,FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  userSearch:string='';
+  productList:WritableSignal<Product[]>=signal<Product[]>([]);
+  productService:ProductService=inject(ProductService);
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+  getAllProducts(){
+    this.productService.getAllProducts().subscribe((res)=>{
+      this.productList.set(res.data);
+      console.log(this.productList());
+    })
+  }
 
 }
