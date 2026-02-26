d23a827723fd5193009ed55384ad2a63b87d45ea
Habibamohamed2004 <hamedhabiba217@gmail.com>
2026-02-26 22:52:33 +0200

    FreshCart

--- src/app/features/components/allorders/allorders.component.ts
+++ src/app/features/components/allorders/allorders.component.ts
@@ -1,8 +1,9 @@
 import { AuthService } from './../../../core/services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { OrderService } from './../../services/order/order.service';
-import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal, PLATFORM_ID } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-allorders',
@@ -14,27 +15,34 @@ export class AllordersComponent implements OnInit {
   orderService:OrderService=inject(OrderService);
   authService:AuthService=inject(AuthService);
   orders: WritableSignal<any[]>=signal([]);
-  isLoading = false;
+  isLoading: WritableSignal<boolean> = signal(false);
+  platformId=inject(PLATFORM_ID)
   ngOnInit(): void {
-    const token = localStorage.getItem('userToken');
-    if (token) {
-      const decoded: any = jwtDecode(token); // decode the JWT
-      if (decoded && typeof decoded.id === 'string') {
-        const userId = decoded.id;
+    if (isPlatformBrowser(this.platformId)) {
+
+      const token = localStorage.getItem('userToken');
+      if (!token) return;
+
+      const decoded: any = jwtDecode(token);
+      const userId = decoded?.id;
+
+      if (userId) {
         this.getUserOrders(userId);
       }
     }
+    
   }
   getUserOrders(userId:string){
-    this.isLoading=true;
+    this.isLoading.set(true);
     this.orderService.getUserOrders(userId).subscribe({
       next:(res)=>{
+        console.log("ORDERS RESPONSE:", res);
         this.orders.set(res);
-        this.isLoading=false;
+        this.isLoading.set(false);
       },
       error:(err)=>{
         console.log(err);
-        this.isLoading=false;
+        this.isLoading.set(false);
       }
     })
   }
